2e0f5e6fa3991c2567aa7768a5fc4880a42448ad
unknown <charles.bowden@gmail.com>
2017-10-11 16:47:39 -0400

    Randomword Loop and more

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Hangman-Game
-Disney Themed Javascript Hangman Game 
+Javascript Hangman Game 
+
+#############################
+Steps to make a Hangman Game#
+#############################
+
+Possible Themes - Disney, Wrestling, Comics, TV Shows, Cartoons
+
+Will need to create a word bank to pull words from at random
+// This will likely be an array of items then use that Math.floor code to generate a random word from the array
+
+
+Will need to figure out how to check user's choice of letter against computer's random choice, then either apply that choice to the "used" letter bank or populate the "answer" fields
+
+
+
+Will need to set a number of guesses
+
+Will need to have a way to check for complete guess and declare a winner.
+
--- hangman.js
+++ hangman.js
@@ -3,15 +3,66 @@
 // Oct. 7-14, 2017
 // By Charles Bowden
 
+// resources: http://apprize.info/javascript/kids/7.html
+//
 
 // Possible Variables
 
 // Word Bank Array
-var superstar = ["Hulk Hogan", "Jim Cornette", "Rowdy Roddy Piper", "Jake The Snake Roberts"]
-};
+var superstar = ["Hulk Hogan", "Jim Cornette", "Rowdy Roddy Piper", "Jake The Snake Roberts"];
 
 // Way to Generate Random Word choice by computer
-var randomWord = Math.floor(Math.random()*superstar.length);
+var randomWord = superstar[Math.floor(Math.random()*superstar.length)];
+
+// creating the answer blanks using an array
+var mysteryWord = [];
+
+for (var i = 0; i < randomWord.length; i++) {
+	mysteryWord[i] = "_";
+}
+
+//Display Blanks for MysteryWord
+
+
+
+var remainingLetters = randomWord.length;
+
+// Game Loop
+while (remainingLetters > 0) {
+	// Game Code
+	// Update Progress
+	document.querySelector("#randomWord").innerHTML = mysteryWord.join(" ");
+
+	// Take input from player
+	document.onkeypress = function(event){
+		var guess = String.fromCharCode(event.keyCode).toLowerCase();
+	}
+	
+		if (lives > 0) {
+			//update the game state
+			for (var j = 0; j < randomWord.length; j++) {
+			if (randomWord[j] === guess) {
+				mysteryWord[j] = guess;
+				remainingLetters--;
+				}
+			}
+		}
+
+	// Update mysteryWord and remainingLetters for every guess
+
+
+};
+
+// Number of Chances Variable
+var lives = 10;
+
+// Variables for scores
+var wins = 0;
+var losses = 0;
+
+
+var guessedLetters = [];
+
 
 
 
@@ -24,6 +75,11 @@ document.onkeypress = function(event) {
 	alert(charStr);
 }
 
+
+// Function that updates the score...
+function updateScore() {
+	document.querySelector("#wins").innerHTML = "Wins: " + wins;
+}
 
 // Way to Limit Choices to letters
 
